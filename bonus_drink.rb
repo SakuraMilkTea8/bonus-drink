2f5a45c06fa69570e1a9951ab6755b9144c50140
Ellie Cornish <elisabeth.cornish@favy.co.jp>
2023-05-31 17:31:51 +0900

    Challenge completed
    The answer is 149 (this should be a multiline message)

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,8 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    free_bottles = (amount - 2)/2.to_f
+    amount + free_bottles.round
   end
+
+  total_count_for(100)
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it "finds the total number of bottles received" do
+
+  expect(BonusDrink.total_count_for(100)).to eq 149
+  end
 end
